8e3afc1078478ad208a855406b522ad36a192f8c
donutAnees <pvmohammedanees2003@gmail.com>
2024-04-07 06:02:34 +0530

    Create Project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundSize: {
+        '200x200': '200px 200px',
+      },
+      backgroundImage: {
+        'hero': "url('./img/hero.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
