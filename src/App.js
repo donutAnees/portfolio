1d0900967f157517ba2fddb02533fe0bc9fa5322
donutAnees <pvmohammedanees2003@gmail.com>
2024-04-07 08:15:26 +0530

    Init App.js and Index.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Root from "./pages/Root";
+import { Suspense } from "react";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
+
+function App() {
+  return (
+    <Suspense fallback={<div>loading...</div>}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
+}
+
+export default App;
