1478fd4baa98c45831377958716fce0b30ecab60
donutAnees <pvmohammedanees2003@gmail.com>
2024-04-10 02:23:11 +0530

    Add ui

---
+++ src/ui/Navbar.js
@@ -0,0 +1,9 @@
+import Navlist from "../components/Navlist";
+
+export default function Navbar() {
+  return (
+      <section className="py-1">
+        <Navlist></Navlist>
+      </section>
+  );
+}
