7b768f6c2be088c4e92a4e85ec0de4ac80d655ea
donutAnees <pvmohammedanees2003@gmail.com>
2024-04-11 19:33:34 +0530

    Add Logo

--- src/components/Navlist.js
+++ src/components/Navlist.js
@@ -1,16 +1,19 @@
-const item = ["File", "Edit", "View", "Special"];
+import image from "../img/logo.png";
+
+const items = ["File", "Edit", "View", "Special"];
 
 export default function Navlist() {
   return (
     <div>
-      <ul className="flex gap-8">
-        {item.map((i, index) => {
-          return (
-            <div key={index} className="font-chicago">
-              {i}
-            </div>
-          );
-        })}
+      <ul className="flex gap-8 align-middle">
+        <li>
+          <img alt="logo" src={image} className="h-6 pl-6"/>
+        </li>
+        {items.map((item, index) => (
+          <li key={index} className="font-chicago">
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
   );
